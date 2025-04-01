410efa8120f6e68dfac50e25f14ec853fb02d761
Andy Kohler <akohler@library.ucla.edu>
2025-04-01 15:32:10 -0800

    Remove entire previous 2.0.3 installation

--- ucla_providence/app/version.php
+++
@@ -1,9 +0,0 @@
-<?php
- 	# CollectiveAccess version number; don't change
-	define('__CollectiveAccess__', '2.0');
-
-	# Schema revision
-	define('__CollectiveAccess_Schema_Rev__', 199);
-
-	# Release type
-	define('__CollectiveAccess_Release_Type__', 'GIT');
